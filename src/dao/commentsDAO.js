4ab74cf211e554c34cda5b0d9951453f35a3c27b
Mykola Maslov <kategorychnost@gmail.com>
2019-02-26 00:28:44 -0500

    User-Facing Backend Basic aggregation, updates, deletes, and join.

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -45,7 +45,8 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: Create/Update Comments
       // Construct the comment document to be inserted into MongoDB.
-      const commentDoc = { someField: "someValue" }
+      let { email, name } = user;
+      const commentDoc = { movie_id: new ObjectId(movieId), name, email, text: comment, date }
 
       return await comments.insertOne(commentDoc)
     } catch (e) {
@@ -70,8 +71,8 @@ export default class CommentsDAO {
       // Use the commentId and userEmail to select the proper comment, then
       // update the "text" and "date" fields of the selected comment.
       const updateResponse = await comments.updateOne(
-        { someField: "someValue" },
-        { $set: { someOtherField: "someOtherValue" } },
+        { email: userEmail, _id: new ObjectId(commentId) },
+        { $set: { text, date} },
       )
 
       return updateResponse
@@ -95,7 +96,7 @@ export default class CommentsDAO {
       // TODO Ticket: Delete Comments
       // Use the userEmail and commentId to delete the proper comment.
       const deleteResponse = await comments.deleteOne({
-        _id: ObjectId(commentId),
+        _id: ObjectId(commentId), email: userEmail
       })
 
       return deleteResponse
